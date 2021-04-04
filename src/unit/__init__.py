4eace6f043d1b36493456b4f0775a6ac7181af6e
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-04-04 20:11:07 +0800

    refactor: make base unit optional

--- src/unit/__init__.py
+++ src/unit/__init__.py
@@ -8,13 +8,16 @@ def trim(value):
 
 
 def split_unit(value):
-    compile = re.compile("{}{}".format(
-        variables.number_regex,
-        variables.alphabet_regex
-    ))
-    match = compile.match(value)
-    if match:
-        return match.groups()
+    if (any(map(str.isalpha, value))):
+        compile = re.compile("{}{}".format(
+            variables.number_regex,
+            variables.alphabet_regex
+        ))
+        match = compile.match(value)
+        if match:
+            return match.groups()
+    else:
+        return [value]
 
 
 def base(value):
@@ -22,7 +25,7 @@ def base(value):
     split = split_unit(clean)
 
     number = float(split[0])
-    suffix = split[1]
+    suffix = split[1] if len(split) == 2 else 'base'
 
     return number * variables.units[suffix]['value']
 
